763df79c4121d0ae28a3ff735f6008fcf74c06d9
vanshu51145 <vanshu51165@gmail.com>
2026-06-24 20:55:14 +0530

    Resolve merge conflict

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,10 +43,7 @@ that help businesses grow in the digital world.
 
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 13c96ebf87c72708f2e6004bea570e5f4bc056a0
 <div className="hero-buttons">
 
 
@@ -87,13 +84,10 @@ duration:1
 }}
 
 >
-<<<<<<< HEAD
  <img
   src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3"
   alt="Developer"
 />
-=======
->>>>>>> 13c96ebf87c72708f2e6004bea570e5f4bc056a0
 
 
 
