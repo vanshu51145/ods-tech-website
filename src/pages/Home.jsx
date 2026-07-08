47b9fea7a19941bfc922f73d1658c7fd7a2ba3ff
vanshu51145 <vanshu51165@gmail.com>
2026-07-08 22:29:16 +0530

    addes FAQ

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,11 @@
-import { useEffect, useState } from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import ServicesSection from "../components/ServicesSection";
-import { Helmet } from "react-helmet-async";
 import Testimonials from "../components/Testimonials";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import React from "react";
-
-import "swiper/css";
+import FAQ from "../components/FAQ";
+import { Helmet } from "react-helmet-async";
 
 function Home() {
-  const [testimonials,setTestimonials] = useState([]);
-
-const [showModal,setShowModal] = useState(false);
-
-
-const [review,setReview]=useState({
-
-clientName:"",
-company:"",
-rating:"",
-feedback:""
-
-});
-useEffect(()=>{
-
-fetch(
-"https://ods-network-backend.onrender.com/api/testimonials"
-)
-
-.then(res=>res.json())
-
-.then(data=>{
-
-setTestimonials(data);
-
-})
-
-.catch(err=>{
-
-console.log(err);
-
-});
-
-
-},[]);
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("https://ods-network-backend.onrender.com/api/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch(() => {});
-  }, []);
-
   return (
     <>
       <Helmet>
@@ -73,9 +24,10 @@ console.log(err);
       <ServicesSection />
 
       <About />
+
       <Testimonials />
 
-
+      <FAQ />
     </>
   );
 }
