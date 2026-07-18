82c7fcfd1c812284fb1c91e081e02abc6512f2a4
vanshu51145 <vanshu51165@gmail.com>
2026-07-18 19:14:20 +0530

    fix the issue ticket.find

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Ticket = require("../models/Ticket");
+const auth = require("../middleware/auth");
 
 router.post("/", async (req, res) => {
   try {
