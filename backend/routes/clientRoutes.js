e65ff8548063e2ee12510d744171a38cbc8b2742
vanshu51145 <vanshu51165@gmail.com>
2026-07-19 17:34:33 +0530

    fix email trigger

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -8,8 +8,8 @@ const Client = require("../models/Client");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
