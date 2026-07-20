28977300f8e37c1ecb014f47a49352c3842a66be
vanshu51145 <vanshu51165@gmail.com>
2026-07-20 20:25:02 +0530

    nodmailer debugging and fix it

--- backend/server.js
+++ backend/server.js
@@ -72,13 +72,32 @@ mongoose
   });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+
+  host: "smtp.gmail.com",
+
+  port: 587,
+
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
 
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+});
+transporter.verify((error, success) => {
+
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+
+});
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
