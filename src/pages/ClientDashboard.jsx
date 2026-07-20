7fdd5f5791ea77ce32fff6de96e9fc43525d01d0
vanshu51145 <vanshu51165@gmail.com>
2026-07-20 22:01:24 +0530

    Client Portal Progress timelines

--- src/pages/ClientDashboard.jsx
+++ src/pages/ClientDashboard.jsx
@@ -12,7 +12,7 @@ function ClientDashboard() {
 
   const logout = () => {
 
-    localStorage.removeItem("token");
+    localStorage.removeItem("clientToken");
     localStorage.removeItem("client");
 
     navigate("/client/login");
