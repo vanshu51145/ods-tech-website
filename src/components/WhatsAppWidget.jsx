541a3aa0f474d915677a9c8037137e9dd30c7eeb
vanshu51145 <vanshu51165@gmail.com>
2026-07-20 19:40:37 +0530

    update whatsapp phone number

--- src/components/WhatsAppWidget.jsx
+++ src/components/WhatsAppWidget.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./WhatsAppWidget.css";
 
 function WhatsAppWidget() {
-  const phone = "919876543210"; // Dummy number
+  const phone = "+91 98013 51931"; // 
   const message = encodeURIComponent(
     "Hi ODS Network, I would like to inquire about your services."
   );
