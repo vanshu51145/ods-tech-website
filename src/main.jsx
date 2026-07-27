dbb67f6d88c7244f53554f45b40144d2e2ad7741
vanshu51145 <vanshu51165@gmail.com>
2026-07-27 22:01:46 +0530

    Sitemap Generation & Robots.txt

--- src/main.jsx
+++ src/main.jsx
@@ -26,4 +26,35 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 );
 
 
+// =========================
+// Register Service Worker
+// =========================
 
+// if ("serviceWorker" in navigator) {
+
+//   window.addEventListener("load", () => {
+
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+
+//       .then((registration) => {
+
+//         console.log(
+//           "Service Worker registered successfully:",
+//           registration.scope
+//         );
+
+//       })
+
+//       .catch((error) => {
+
+//         console.error(
+//           "Service Worker registration failed:",
+//           error
+//         );
+
+//       });
+
+//   });
+
+// }
